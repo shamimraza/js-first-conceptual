f92eaec7d23cd89e370422fd93a29122c6f2eac8
Shamim raza <shamimraza411@gmail.com>
2023-08-01 23:49:34 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,124 @@
+// var x = 34.77;
+// var result = parseInt(x);
+// console.log(result);
+
+1. // Number converction
+
+// var num = '15.666778';
+// var num1 = parseFloat(num);
+// var int = parseInt(num1);
+// var fixed = num1.toFixed(2);
+// console.log( parseFloat(fixed));
+
+2. //  (+ - * /)
+
+// var x = 12;
+// console.log(++x);
+
+
+3. //  array length 
+
+// var arr = [10, 23, 43, 56, 34, 88,80]
+// var Number = arr.length;
+// var section = arr[3];
+// console.log(section);
+
+4. // push and pop 
+
+// var name = ['nadia', 'rumps', 'anna']
+// console.log(name);
+// name.push('lovely')
+// console.log(name);
+// name.pop()
+// console.log(name);
+
+5. // if else condition
+
+// var age = 25;
+
+// if (age >= 18) {
+//     console.log('you are adult');
+// }
+// else{
+//     console.log('you are not adult');
+// }
+
+// grading system
+
+// var mark = 67;
+
+// if (mark>= 80) {
+//     console.log('you got A+');
+// }
+// else if (mark >= 70) {
+//     console.log('you got b');
+// }
+// else if (mark >= 60) {
+//     console.log('you got A-');
+// }
+// else{
+//     console.log('you got c');
+// }
+
+// var temperature = 25;
+// var isRaining = false;
+
+// if (temperature > 30) {
+//     console.log('it is hot day');
+//     if (isRaining === true) {
+//         console.log('it is raining');
+//     }
+//     else{
+//         console.log('it is not raining');
+//     }
+// }
+// else{
+//     console.log('it is cold day');
+//     if (isRaining === true) {
+//         console.log('it is raining');
+//     }
+//     else{
+//         console.log('it is not raining');
+//     }
+// }
+
+
+6.  //   loop ... for loop   wheel loop 
+
+// var i  =  1;
+
+// while (i <= 10) {
+//     console.log('sorry', i);
+//     i++;
+// }
+
+// for (var i =1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+ 7. //brake condition / continue.
+
+//  var arr = [10, 20, 30, 40, 50, 60, 70, 80];
+
+//  for (var i =0; i < arr.length; i++){
+//     console.log(arr[i]);
+//     if (arr[i] === 30) {
+//         break;
+//     }
+
+//  }
+
+8. // 
+
+// var raining = true;
+// var umbrella = true;
+
+// if (raining === true || umbrella === false) {
+//     console.log('baira jabo');
+// }
+// else{
+//     console.log('jabo na baira');
+// }
+
+9. //
